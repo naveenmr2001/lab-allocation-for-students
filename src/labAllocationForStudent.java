bd6505c4aff8715c95497358df70d3a8473ecb2d
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-23 20:05:36 +0530

    Update. Error Forget to do Git add in the file So Add logic to lab allocation of student

--- src/labAllocationForStudent.java
+++ src/labAllocationForStudent.java
@@ -1,5 +1,52 @@
-public class labAllocationForStudet {
+import java.util.Scanner;
+public class labAllocationForStudent {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the Lab1 Space: ");
+        int lab1Space = scan.nextInt();
+
+        System.out.println("Enter the Lab2 Space: ");
+        int lab2Space = scan.nextInt();
+
+        System.out.println("Enter the Lab3 Space: ");
+        int lab3Space = scan.nextInt();
+
+        System.out.println("Enter the Number of Student: ");
+        int numberOfStudents = scan.nextInt();
+
+        int[] differenceOfLabSpace = new int[]{lab1Space,lab2Space,lab3Space};
+
+        int minimumSpacedLab = Integer.MAX_VALUE;
+
+        for(int iterator = 0;iterator<differenceOfLabSpace.length;iterator++){
+
+            if((differenceOfLabSpace[iterator]-numberOfStudents)>=0){
+
+                int differenceOfLabSpaceAndStudentSpace = Math.abs(differenceOfLabSpace[iterator]-numberOfStudents);
+
+                minimumSpacedLab = Math.min(minimumSpacedLab,differenceOfLabSpaceAndStudentSpace);
+
+                differenceOfLabSpace[iterator] = differenceOfLabSpaceAndStudentSpace;
+
+            }
+        }
+
+        int minimumAllocationOfLab = 0;
+
+        for(int iterator = 0;iterator<differenceOfLabSpace.length;iterator++){
+
+            if(differenceOfLabSpace[iterator] == minimumSpacedLab){
+
+                minimumAllocationOfLab = iterator;
+
+                break;
+
+            }
+        }
+
+        System.out.println("L"+(minimumAllocationOfLab+1));
+
     }
 }
